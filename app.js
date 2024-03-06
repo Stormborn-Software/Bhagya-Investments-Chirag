2d7f4b91b75d7c0906bf3b22e2c8848ecf449be7
Sapeksh Pareek <sapekshpareek136@gmail.com>
2024-03-06 17:10:23 +0530

    Data Init

--- app.js
+++ app.js
@@ -5,29 +5,30 @@ const ejsmate = require("ejs-mate");
 const homeRouter = require("./routes/home.js");
 const panchangRouter = require("./routes/panchang.js");
 const pujaRouter = require("./routes/puja.js");
-const puja1Router = require("./routes/puja.js")
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/devpr1";
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.set("view engine", 'ejs');
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-//app.use(express.urlencodedz({ extended: true}));
+//app.use(express.urlencoded({ extended: true}));
 app.engine("ejs", ejsmate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-    res.send("Hi, I am Root");
-  });
+  res.send("Hi, I am Root");
+});
 
 app.use("/home", homeRouter);
 app.use("/panchang", panchangRouter);
 app.use("/puja", pujaRouter);
-app.use("/puja1", puja1Router)
-  
-app.listen(8080, () => {
-    console.log("Listening to Port: http://127.0.0.1:8080/home");
-``  });
 
-// // Index Route
-// app.get("/puja", (req, res) =>{
-//   res.render("puja/index.ejs")
-// });
+app.listen(8080, () => {
+  console.log("Listening to Port: http://127.0.0.1:8080/home");
+});
---
+++ init/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Puja = require("../models/puja.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/devpr1";
+main()
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Puja.insertMany(initData.data);
+  console.log("Data Initialized");
+};
+
+initDB();
