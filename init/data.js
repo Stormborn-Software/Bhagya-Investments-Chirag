d2a8429732210ed205883161f9789cf19b27b29a
Sarthaknagarjii <sarthaknagarjii@gmail.com>
2024-03-10 17:55:47 +0530

    Changes in DB

--- init/data.js
+++ init/data.js
@@ -1,13 +1,17 @@
 const pujaListings = [
     {
-        "title": "Ganesh Puja",
-        "description": "A puja dedicated to Lord Ganesh, the remover of obstacles.",
-        "price": 50.00
+        title: "Ganesh Puja",
+        description: "A puja dedicated to Lord Ganesh, the remover of obstacles.",
+        image: {
+            url: "[Image url]"
+        }
     },
     {
-        "title": "Ganesh Puja",
-        "description": "A puja dedicated to Lord Ganesh, the remover of obstacles.",
-        "price": 50.00
+        title: "Ganesh Puja",
+        description: "A puja dedicated to Lord Ganesh, the remover of obstacles.",
+        image: {
+            url: "[Image url]"
+        }
     },
 ];
  module.exports = {data: pujaListings};
