a9d68bcc3bd04674f0d70b87816f7371807fcedc
Sarthaknagarjii <sarthaknagarjii@gmail.com>
2024-03-11 01:17:51 +0530

    Added Images URL

--- init/data.js
+++ init/data.js
@@ -1,19 +1,75 @@
 const pujaListings = [
     {
-        title: "Ganesh Puja",
-        description: "A puja dedicated to Lord Ganesh, the remover of obstacles.",
+        title: "Maha Rudra Abhishek",
+        description: "Maha Rudrabhisheka, a revered ritual for Lord Shiva, utilizes honey, curd, ghee, and milk, along with sacred chants to purify and remove sins, negating planetary afflictions.",
         image: {
-            url: "[Image url]",
-            name: "Ganesh Puja"
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098950/Dev_Pr1/mffbzsqur36d2spposey.jpg ",
+            name: "Maha Rudra Abhishek"
         }
     },
     {
-        title: "Ganesh Puja",
-        description: "A puja dedicated to Lord Ganesh, the remover of obstacles.",
+        title: "Mangal Bhat Puja",
+        description: "Mangal Bhaat Puja addresses issues arising from anger, relationship strains, financial troubles, and accidents. At Mangalnath temple, devotees perform puja for Mangal Dosha, believed to affect marriage due to unfavorable Mars placement in the Janam Kundli.",
         image: {
-            url: "[Image url]",
-            name: "Ganesh PUja"
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098951/Dev_Pr1/cswanskhiuozrxylcusg.jpg",
+            name: "Mangal Bhat Puja"
         }
     },
+    {
+        title: "Maha Mritunjay Jab",
+        description: "Maha Mrityunjaya Japa is a powerful Hindu ritual involving repetitive chanting of the Maha Mrityunjaya Mantra, dedicated to Lord Shiva, for overcoming fear of death and ensuring longevity..",
+        image: {
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098956/Dev_Pr1/umxe318nhzkbcdbcjisb.jpg",
+            name: "Maha Mritunjay Jab"
+        }
+    },
+    {
+        title: "Kalsarp Dosh Puja",
+        description: "Kalsarp Dosh Puja is a Hindu ritual performed to mitigate the adverse effects of Kalsarp Dosh, a planetary alignment believed to cause obstacles and hardships in life.",
+        image: {
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098955/Dev_Pr1/va3rsv1raubpqyowx9r6.jpg",
+            name: "Kalsarp Dosh Puja"
+        }
+    },
+    {
+        title: "Navgrah Shanti",
+        description: "Navgrah Shanti is a Hindu ritual aimed at pacifying the nine celestial bodies (Navgrahas) to alleviate astrological afflictions and foster harmony, prosperity, and well-being in one's life..",
+        image: {
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098951/Dev_Pr1/qskjppceqzu6t7hf1u9n.jpg",
+            name: "Navgrah Shanti"
+        }
+    },
+    {
+        title: "Durga Path",
+        description: "Durga Path is a devotional practice in Hinduism involving recitation of sacred verses dedicated to Goddess Durga, seeking her blessings for protection, strength, and prosperity.",
+        image: {
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098950/Dev_Pr1/ehlxz5zfutxhnfex52rs.jpg",
+            name: "Durga Path"
+        }
+    },
+    {
+        title: "Laghu Rudra Abhishek",
+        description: "A holy Hindu ceremony called Laghu Rudra Abhishek is performed in honor of Lord Shiva. It includes the recitation of Rudram and the giving of water, milk, and bilva leaves in exchange for heavenly favors.",
+        image: {
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098950/Dev_Pr1/mffbzsqur36d2spposey.jpg",
+            name: "Laghu Rudra Abhishek"
+        }
+    },
+    {
+        title: "Pitra Dosh Puja",
+        description: "A Hindu ceremony called Pitra Dosh Puja seeks to please the ancestors, lift ancestral curses or afflictions, and grant blessings for harmony and wealth within the family.",
+        image: {
+            url: "https://res.cloudinary.com/daw4ojh5s/image/upload/v1710098951/Dev_Pr1/hmdph8bwbworyboyxmv0.jpg",
+            name: "Pitra Dosh Puja"
+        }
+    },
+
+
 ];
  module.exports = {data: pujaListings};
+
+
+
+
+
+
